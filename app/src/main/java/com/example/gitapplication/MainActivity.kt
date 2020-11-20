e43995c6cf27929c115c1da8e26fb76b34b11a97
Godwill Junior <jamesgodwillarkoh@gmail.com>
2020-11-20 17:50:14 +0000

    added a toast to LatestWork

--- app/src/main/java/com/example/gitapplication/MainActivity.kt
+++ app/src/main/java/com/example/gitapplication/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.gitapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(applicationContext,"Added Toast to Git Project",Toast.LENGTH_LONG).show()
     }
 }
